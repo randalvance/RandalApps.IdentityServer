c9f7a97f3c9bdbc1c6c9d0fe0923a572e1ca78a9
Randal Vance Cunanan <randal@randalvance.net>
2016-10-16 14:52:20 +0800

    Data Seed changes

--- src/RandalApps.IdentityServer/Data/DataSeeder.cs
+++ src/RandalApps.IdentityServer/Data/DataSeeder.cs
@@ -36,9 +36,9 @@ namespace RandalApps.IdentityServer.Data
             {
                 new IdentityServerEF.Scope()
                 {
-                    Name = "CentBucket.Api",
-                    DisplayName = "CentBucket API",
-                    Description = "Access to CentBucket API",
+                    Name = "QuizMaster.Api",
+                    DisplayName = "QuizMaster API",
+                    Description = "Access to QuizMaster API",
                     Enabled = true,
                     Type = (int)ScopeType.Resource
                 },
@@ -73,7 +73,7 @@ namespace RandalApps.IdentityServer.Data
                 new IdentityServerEF.Client()
                 {
                     ClientId = "97008f0b-2922-4819-9e38-78951bbe5c9",
-                    ClientName = "CentBucket.Web",
+                    ClientName = "QuizMaster.Admin",
                     ClientSecrets = new List<IdentityServerEF.ClientSecret>
                     {
                         new IdentityServerEF.ClientSecret() { Value = "?V-WwrWHdb*R3Q%gF4X_gZX9pHV#kyU^V+Kyr&3NHHr@aT-DMyq3ydnQwz-S+vr7f^tW+K%+pyZ+6".Sha256() }
@@ -102,38 +102,8 @@ namespace RandalApps.IdentityServer.Data
                 },
                 new IdentityServerEF.Client()
                 {
-                    ClientId = "7ab41cdd-26f0-4f6f-b610-d46c8089ecb4",
-                    ClientName = "CentBucket.MobileBackend",
-                    ClientSecrets = new List<IdentityServerEF.ClientSecret>
-                    {
-                        new IdentityServerEF.ClientSecret() { Value = "Yy6KS5cL7gPBSmhx#EKJ#rMy^+&^s^kjWLDSnN8SDRUB3M4$dJ@syJaRskVez+FQ=d5jfg2cZUBk6".Sha256() }
-                    },
-                    Enabled = true,
-                    AllowedGrantTypes = new List<IdentityServerEF.ClientGrantType>()
-                    {
-                        hybridGrantType,
-                        clientCredentialsGrantType
-                    },
-                    AllowedScopes = scopes.Select(s => new IdentityServerEF.ClientScope() { Scope = s.Name }).ToList(),
-                    RequireConsent = true,
-                    RedirectUris = new List<IdentityServerEF.ClientRedirectUri>()
-                    {
-                        new IdentityServerEF.ClientRedirectUri() { RedirectUri = "http://localhost:1111/signin-oidc" }
-                    },
-                    PostLogoutRedirectUris = new List<IdentityServerEF.ClientPostLogoutRedirectUri>()
-                    {
-                        new IdentityServerEF.ClientPostLogoutRedirectUri() { PostLogoutRedirectUri = "http://localhost:1111" }
-                    },
-                    // The following are in seconds as I verified in the IdentityServer code
-                    // Default to 1 hour
-                    AccessTokenLifetime = 3600,
-                    AuthorizationCodeLifetime = 3600,
-                    IdentityTokenLifetime = 3600
-                },
-                new IdentityServerEF.Client()
-                {
                     ClientId = "524bdbb8-19f6-471b-a969-19ec3a0d6654",
-                    ClientName = "CentBucket.Android",
+                    ClientName = "QuizMaster.Droid",
                     ClientSecrets = new List<IdentityServerEF.ClientSecret>
                     {
                         new IdentityServerEF.ClientSecret() { Value = "AQ@XDdn&LvW6Y$B-uBXvve#_L^sTTvhj#yP&fu*%p99ZQn2Jr6^H%S2jtnJ#E=gC8gD5*T-4w9M3@".Sha256() }
@@ -141,17 +111,17 @@ namespace RandalApps.IdentityServer.Data
                     Enabled = true,
                     AllowedGrantTypes = new List<IdentityServerEF.ClientGrantType>()
                     {
-                        resourceOwnerGrantType
+                        authCodeGrantType
                     },
                     AllowedScopes = scopes.Select(s => new IdentityServerEF.ClientScope() { Scope = s.Name }).ToList(),
                     RequireConsent = true,
                     RedirectUris = new List<IdentityServerEF.ClientRedirectUri>()
                     {
-                        new IdentityServerEF.ClientRedirectUri() { RedirectUri = "http://codequasar.com/redirect" }
+                        new IdentityServerEF.ClientRedirectUri() { RedirectUri = "http://apps.randalvance.net/redirect" }
                     },
                     PostLogoutRedirectUris = new List<IdentityServerEF.ClientPostLogoutRedirectUri>()
                     {
-                        new IdentityServerEF.ClientPostLogoutRedirectUri() { PostLogoutRedirectUri = "http://codequasar.com/logout-redirect" }
+                        new IdentityServerEF.ClientPostLogoutRedirectUri() { PostLogoutRedirectUri = "http://apps.randalvance.net/logout-redirect" }
                     },
                     AllowAccessTokensViaBrowser = true,
                     // The following are in seconds as I verified in the IdentityServer code
--- src/RandalApps.IdentityServer/Program.cs
+++ src/RandalApps.IdentityServer/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace RandalApps.IdentityServer
 {
@@ -11,7 +12,15 @@ namespace RandalApps.IdentityServer
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("hosting.json", optional: true)
+                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                .AddCommandLine(args)
+                .Build();
+
             var host = new WebHostBuilder()
+                .UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
